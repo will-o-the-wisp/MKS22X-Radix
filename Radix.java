7608014233faa38170c5af763ccfcdab16fb439c
will-o-the-wisp <kli18@stuy.edu>
2019-04-02 10:39:50 -0400

    wrote testing methods

--- Radix.java
+++ Radix.java
@@ -1,5 +1,10 @@
 import java.util.*;
 public class Radix{
+  public static void main(String[] args){
+    int[] data = {11,32,35,50,99};
+    radixsort(data);
+    System.out.println(data);
+  }
   public static void radixsort(int[] data){
     if(data.length==0){
       return;
@@ -11,10 +16,23 @@ public class Radix{
       }
     }
     int maxd = (int)Math.log10(Math.abs(max))+1;
-    ArrayList<MyLinkedList<Integer>> buckets = new ArrayList<MyLinkedList<Integer>>();
-    for(int i=0;i<10;i++){
-      buckets.add(new MyLinkedList<Integer>());
+    MyLinkedList<Integer>[] buckets = new MyLinkedList[10];
+    for(int i=0;i<data.length;i++){
+      int d = data[i]%10;
+      buckets[d].add(data[i]);
     }
-    
+  }
+  private static void printBuckets(MyLinkedList<Integer>[] b){
+    for(int i=0;i<b.length;i++){
+      System.out.println(b[i]);
+    }
+  }
+  private static String ats(int[] a){
+    String ans="";
+    for(int i=0;i<a.length;i++){
+      ans+=a[i];
+      ans+=" ";
+    }
+    return ans;
   }
 }
